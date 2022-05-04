7c9629267581e3469b2518ef5de2672ec01a603d
shubhit304 <80594272+shubhit304@users.noreply.github.com>
2022-05-04 14:40:13 +0530

    Add files via upload

---
+++ Test.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Test.Domain.Interfaces;
+using Test.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+
+        public IUserRepo Users { get; private set; }
+
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+            Users = new UserRepo(_context);
+        }
+
+        public UnitOfWork(ApplicationContext context, IUserRepo paramUserRepo)
+        {
+            _context = context;
+            Users = paramUserRepo;
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Test.WebAPI/Program.cs
@@ -0,0 +1,35 @@
+using Test.Domain.Interfaces;
+using Test.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region Repositories
+//builder.Services.AddTransient(typeof(IGenericRepo<>), typeof(GenericRepo<>));
+builder.Services.AddRepository();
+
+#endregion
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
