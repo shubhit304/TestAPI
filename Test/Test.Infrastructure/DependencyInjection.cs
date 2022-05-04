6c1dbb4b4119ee4c4fe4c139faf9858604205616
shubhit304 <80594272+shubhit304@users.noreply.github.com>
2022-05-04 14:35:22 +0530

    Add files via upload

---
+++ Test/Test.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using Test.Domain.Interfaces;
+using Test.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        //private static readonly IConfiguration _configuration;
+
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+            //services.AddTransient<IProductRepository, ProductRepository>();
+            //services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IUserRepo, UserRepo>();
+
+            //services.AddMemoryCache();
+
+            //services.AddDbContext<ApplicationContext>(opt => opt
+            //    .UseSqlServer("Server=DESKTOP-UUBJ14C\\SQLEXPRESS; Database=OrderDb;Trusted_Connection=True;"));
+            //services.AddDbContext<ApplicationContext>(options => options.UseOracle(_configuration.GetConnectionString("OracleConn")));
+            return services;
+        }
+
+        //public static IServiceCollection AddServices(this IServiceCollection services)
+        //{
+        //    return services
+        //        .AddScoped();
+        //}
+    }
+}
---
+++ Test/Test.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Test.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.WebAPI.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult GetAll()
+        {
+            var allUsers = _unitOfWork.Users.GetAll();
+            return Ok(allUsers);
+        }
+    }
+}
